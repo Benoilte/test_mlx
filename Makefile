7fa8bfa8ca81db741f66bf4ef8b45d1cd861f90b
benoit brandt <benoit.07@hotmail.com>
2024-01-21 12:51:29 +0100

    update makefile and main.c for linux computer

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/12/04 11:59:35 by bebrandt          #+#    #+#              #
-#    Updated: 2024/01/21 11:27:17 by bebrandt         ###   ########.fr        #
+#    Updated: 2024/01/21 12:37:55 by bebrandt         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -35,29 +35,31 @@ UNAME = $(shell uname)
 MLX_LIB = $(MLX_DIR)/libmlx.a
 
 ifeq ($(UNAME), Linux)
-	INCLUDES = -I/usr/include -Iminilibx-linux
+	INCLUDES = -I/usr/include -Iminilibx-linux -Ilibft
 	MLX_DIR = ./minilibx-linux
 else
-	INCLUDES = -Iminilibx_macos
+	INCLUDES = -Iminilibx_macos -Ilibft
 	MLX_DIR = ./minilibx_macos
 endif
- 
+
 ifeq ($(shell uname), Linux)
-	MLX_FLAGS = -Lmlx -lmlx -L/usr/lib/X11 -lXext -lX11
+	MLX_FLAGS = -L$(MLX_DIR) -lmlx -L/usr/lib/X11 -lXext -lX11
 else
 	MLX_FLAGS = -L$(MLX_DIR) -lmlx -framework OpenGL -framework AppKit
 endif
- 
+
 RED			=	\033[0;31m
 GREEN		=	\033[0;32m
 NONE		=	\033[0m
 
-all: $(MLX_LIB) $(NAME)
- 
+all: $(MLX_LIB) $(LIBFT_DIR)$(LIBFT_NAME) $(NAME)
+
 $(NAME): $(MLX_TEST_OBJ)
-	@make -C $(LIBFT_DIR) --silent
-	$(CC) $(CFLAGS) -o $(NAME) $(OBJS) -L$(LIBFT_DIR) -lft $(MLX_FLAGS) -lm $(MLX_TEST_SRCS)
- 
+	$(CC) $(CFLAGS) -o $(NAME) $(MLX_TEST_OBJ) -L$(LIBFT_DIR) -lft $(MLX_FLAGS)
+
+$(LIBFT_DIR)$(LIBFT_NAME):
+	@make -C $(LIBFT_DIR)
+
 $(OBJ_DIR)%.o: %.c
 	@mkdir -p $(OBJ_DIR)
 	@echo "$(GREEN)##### Creating" [ $@ ] " #####$(NONE)"
@@ -65,14 +67,16 @@ $(OBJ_DIR)%.o: %.c
 
 $(MLX_LIB):
 	@make -C $(MLX_DIR)
-	
+
 clean:
 	@make -C $(LIBFT_DIR) clean  --silent
+	@make -C $(MLX_DIR) clean
 	@rm -f $(MLX_TEST_OBJ)
 	@echo "$(RED)##### Removed object files #####$(NONE)"
 
 fclean: clean
 	@make -C $(LIBFT_DIR) fclean  --silent
+	@make -C $(MLX_DIR) clean
 	@rm -f $(NAME)
 	@echo "$(RED)##### Removed binary files #####$(NONE)"
 
--- main.c
+++ main.c
@@ -6,11 +6,12 @@
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 17:48:31 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/01/20 16:59:55 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/01/21 12:50:41 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minilibx_macos/mlx.h"
+// #include "minilibx_macos/mlx.h"
+#include "minilibx-linux/mlx.h"
 #include "libft/libft.h"
 
 typedef struct s_data {
@@ -36,14 +37,10 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 void	print_square(t_data *img, int start_x, int start_y, int color, int size)
 {
-	int	max_height;
-	int	max_length;
 	int	buff_size;
 	int	x;
 	int	y;
 
-	max_height = start_y + size;
-	max_length = start_x + size;
 	x = start_x;
 	y = start_y;
 	buff_size = size;
@@ -79,7 +76,7 @@ void	display_square(t_vars *vars, int color)
 {
 	t_data	img;
 
-	img.img = mlx_new_image(vars->mlx_ptr, 1920, 1080);
+	img.img = mlx_new_image(vars->mlx_ptr, 500, 500);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 					&img.endian);
 	print_square(&img, 5, 5, color, 100);
@@ -90,7 +87,7 @@ int	ft_mlx_close(t_vars *vars)
 {
 	ft_printf("mlx_ptr in close%p\n", vars->mlx_ptr);
 	mlx_destroy_window(vars->mlx_ptr, vars->win_ptr);
-	exit(0);
+	exit (0);
 }
 
 int	mouse_move(int x, int y, void *param)
@@ -117,13 +114,14 @@ int	mouse_exit_hook(t_vars *param)
 
 int	key_hook(int key, void *param)
 {
-	if (key == 15)
+	ft_printf("key: %d\n", key);
+	if (key == 15 || key == 114)
 		display_square((t_vars *)param, 0xFF0000);
-	if (key == 5)
+	if (key == 5 || key == 103)
 		display_square((t_vars *)param, 0x00FF00);
-	if (key == 11)
+	if (key == 11 || key == 98)
 		display_square((t_vars *)param, 0x0000FF);
-	if (key == 53)
+	if (key == 53 || key == 65307)
 		ft_mlx_close((t_vars *)param);
 	return (0);
 }
@@ -146,13 +144,13 @@ int	main(void)
 	t_vars	vars;
 
 	vars.mlx_ptr = mlx_init();
-	vars.win_ptr = mlx_new_window(vars.mlx_ptr, 1920, 1080, "Heyllo world!");
+	vars.win_ptr = mlx_new_window(vars.mlx_ptr, 500, 500, "Heyllo world!");
 	ft_printf("mlx_ptr in main %p\n", vars.mlx_ptr);
 	mlx_key_hook(vars.win_ptr, &key_hook, &vars);
 	mlx_mouse_hook (vars.win_ptr, &mouse_hook, &vars);
 	mlx_hook(vars.win_ptr, 17, 1L << 19, &ft_mlx_close, &vars);
 	mlx_hook(vars.win_ptr, 6, 1L << 6, &mouse_move, &vars);
-	mlx_hook(vars.win_ptr, 7, 1L << 6, mouse_enter_hook, &vars);
-	mlx_hook(vars.win_ptr, 8, 1L << 6, mouse_exit_hook, &vars);
+	mlx_hook(vars.win_ptr, 7, 1L << 4, mouse_enter_hook, &vars);
+	mlx_hook(vars.win_ptr, 8, 1L << 5, mouse_exit_hook, &vars);
 	mlx_loop(vars.mlx_ptr);
 }
